ede19b92d169259986ae947a70bce7289fbf422c
non <chenry@mcs.anl.gov>
2015-05-16 01:02:32 -0500

    Updates to scripts to permit calling of functions in scripts and in ProbModelSEED server.

--- scripts/App-ModelReconstruction.pl
+++ scripts/App-ModelReconstruction.pl
@@ -21,9 +21,17 @@ eval {
 };
 
 my $script = Bio::KBase::AppService::AppScript->new(\&reconstruct_model);
-
-my $helper = Bio::ModelSEED::ProbModelSEED::ProbModelSEEDHelper->new();
+print 
+my $helper = Bio::ModelSEED::ProbModelSEED::ProbModelSEEDHelper->new({
+	app => $script,
+	token => $script->token()->token(),
+	username => $script->token()->user_id(),
+	method => "ModelReconstruction",
+	run_as_app => 1
+});
+$helper->load_from_config();
 $script->{workspace_url} = $helper->workspace_url();
+$script->{donot_create_result_folder} = 1;
 
 my $rc = $script->run(\@ARGV);
 
@@ -32,57 +40,6 @@ exit $rc;
 sub reconstruct_model
 {
     my($app, $app_def, $raw_params, $params) = @_;
-
     print "Reconstructing model ", Dumper($app_def, $raw_params, $params);
-
-    my $json = JSON::XS->new->pretty(1);
-    
-    my $input = {
-    	app => $app
-    };
-    if (defined($params->{adminmode})) {
-    	$input->{adminmode} = $params->{adminmode}
-    }
-    
-    my $genome;
-    if (defined($params->{reference_genome})) {
-    	$genome = $helper->retreive_reference_genome($params->{reference_genome});
-    } else {
-    	$genome = $helper->get_object($params->{genome},"genome");
-    }
-    
-    if (!defined($genome)) {
-    	$helper->error("Genome retrieval failed!");
-    }
-
-    my $template;
-    if (!defined($params->{templatemodel})) {
-    	if ($genome->domain() eq "Plant" || $genome->taxonomy() =~ /viridiplantae/i) {
-    		$template = $helper->get_object("/chenry/public/modelsupport/templates/plant.modeltemplate","modeltemplate");
-    	} else {
-    		my $classifier_data = $helper->get_object("/chenry/public/modelsupport/classifiers/gramclassifier.string","string");
-    		my $class = $helper->classify_genome($classifier_data,$genome);
-    		if ($class eq "Gram positive") {
-	    		$template = $helper->get_object("/chenry/public/modelsupport/templates/GramPositive.modeltemplate","modeltemplate");
-	    	} elsif ($class eq "Gram negative") {
-	    		$template = $helper->get_object("/chenry/public/modelsupport/templates/GramNegative.modeltemplate","modeltemplate");
-	    	}
-    	}
-    } else {
-    	$template = $helper->get_object($params->{templatemodel},"modeltemplate");
-    }
-    
-    if (!defined($template)) {
-    	$helper->error("template retrieval failed!");
-    }
-    
-    my $mdl = $template->buildModel({
-	    genome => $genome,
-	    modelid => $params->{output_file},
-	    fulldb => $params->{fulldb}
-	});
-    
-    $helper->save_object($app->result_folder()."/".$params->{output_file}.".model",$mdl,"model");
-    $helper->save_object($app->result_folder()."/fba",undef,"folder");
-    $helper->save_object($app->result_folder()."/gapfilling",undef,"folder");
+	$helper->ModelReconstruction($params);
 }
