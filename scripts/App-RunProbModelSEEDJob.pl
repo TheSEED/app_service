9b8858a3a61f9d3ef6365ecde2cf4f9da7933d49
Christopher Henry <chenry@mcs.anl.gov>
2016-01-13 16:12:46 -0600

    Updates to probmodelseed job

--- scripts/App-RunProbModelSEEDJob.pl
+++ scripts/App-RunProbModelSEEDJob.pl
@@ -15,13 +15,17 @@ eval {
 };
 
 my $script = Bio::KBase::AppService::AppScript->new(\&run_probmodelseed_job);
-my $config = Bio::KBase::ObjectAPI::utilities::load_config({service => "ProbModelSEED"});
-$config->{token} = $script->token()->token();
-$config->{username} = $script->token()->user_id();
-$config->{cache_targets} = [split(/;/,$config->{cache_targets})];
-$config->{method} = "ModelReconstruction";
-my $helper = Bio::ModelSEED::ProbModelSEED::ProbModelSEEDHelper->new($config);
-$script->{workspace_url} = $config->{"workspace-url"};
+Bio::KBase::ObjectAPI::config::load_config({
+	filename => $ENV{KB_DEPLOYMENT_CONFIG},
+	service => "ProbModelSEED"
+});
+Bio::KBase::ObjectAPI::logging::log("App starting! Current configuration parameters loaded:\n".Data::Dumper->Dump([Bio::KBase::ObjectAPI::config::all_params()]));
+my $helper = Bio::ModelSEED::ProbModelSEED::ProbModelSEEDHelper->new({
+	token => $script->token()->token(),
+	username => $script->token()->user_id(),
+	method => "RunProbModelSEEDJob",
+});
+$script->{workspace_url} = Bio::KBase::ObjectAPI::config::workspace_url();
 $script->{donot_create_result_folder} = 1;
 $script->{donot_create_job_result} = 1;
 
@@ -34,6 +38,7 @@ sub run_probmodelseed_job
     my($app, $app_def, $raw_params, $params) = @_;
     print "Running job: ", Dumper($app_def, $raw_params, $params);
 	my $command = $params->{command};
+	Bio::KBase::ObjectAPI::config::method($command);
 	my $args = $params->{arguments};
 	if (!ref($args)) {
 		$args = Bio::KBase::ObjectAPI::utilities::FROMJSON($args);
