61d21c6bd76f2ccad50488806144f72d1355ea75
Andrew Warren <aswarren@gmail.com>
2018-05-31 17:36:40 -0400

    rna-seq copy json files into diffexp object

--- scripts/App-RNASeq.pl
+++ scripts/App-RNASeq.pl
@@ -85,6 +85,18 @@ sub process_rnaseq {
 	{
 	    warn "error creating $path: $@";
 	}
+    else {
+        my $type ="txt";
+        for my $ofile (glob("$path/*json"))
+            {
+	        my $filename = basename($ofile);
+            print STDERR "Output folder = $path\n";
+            print STDERR "Saving $ofile => $output\_$filename ...\n";
+            $app->workspace->save_file_to_file("$ofile", {}, "$path/$filename", $type, 1,
+                            (-s "$ofile" > $shock_cutoff ? 1 : 0), # use shock for larger files
+                            $global_token);
+            }
+    }
     }
     for my $output (@outputs)
     {
