877a1ab79df9a6013c3f85e1b06293c94f4aeffa
Robert Olson <olson@mcs.anl.gov>
2018-10-17 16:27:36 -0500

    Add binner spader script.

---
+++ scripts/p3x-run-spades-for-binning.pl
@@ -0,0 +1,79 @@
+use Data::Dumper;
+use strict;
+use Getopt::Long::Descriptive;
+use JSON::XS;
+use File::Slurp;
+use File::Temp;
+use File::Basename;
+
+#
+# Helper script for offloading binning assembly to cluster.
+#
+
+my($opt, $usage) = describe_options("%c %o output-ws-path < params",
+				    ["threads=i", "thread count", { default => 12 }],
+				    ["memory=i", "memory use in GB", { default => 128 }],
+				    ["help|h" => "show this help message."]);
+print($usage->text), exit 0 if $opt->help;
+die($usage->text) unless @ARGV == 1;
+
+my $output_ws = shift;
+
+my $txt = read_file(\*STDIN);
+
+my $doc = decode_json($txt);
+
+print Dumper($doc);
+
+my $workdir = File::Temp->newdir();
+
+my($read1, $read2);
+if ($doc->{read1})
+{
+    my $base1 = basename($doc->{read1});
+    $read1 = "$workdir/$base1";
+    run("p3-cp", "ws:" . $doc->{read1}, $read1);
+
+    my $base2 = basename($doc->{read2});
+    $read2 = "$workdir/$base2";
+    run("p3-cp", "ws:" . $doc->{read2}, $read2);
+}
+print "reads: $read1 $read2\n";
+
+my(@cmd) = ("spades.py",
+	    "-1", $read1, "-2", $read2,
+	    "--meta",
+	    "-o", $workdir,
+	    "--threads", $opt->threads,
+	    "--memory", $opt->memory,
+	    );
+
+print "@cmd\n";
+
+my $rc = system(@cmd);
+
+if ($rc != 0)
+{
+    die "Failed to run spades: rc=$rc\n";
+}
+
+#
+# Run is done, copy contigs and logs back to workspace.
+#
+
+my @map = ("-m", "fasta=contigs", "-m", "log=txt");
+my @files = qw(contigs.fasta spades.log params.txt);
+for my $f (@files)
+{
+    if (-f "$workdir/$f")
+    {
+	run("p3-cp", @map, "$workdir/$f", "ws:$output_ws/$f");
+    }
+}
+
+sub run
+{
+    my(@cmd) = @_;
+    my $rc = system(@cmd);
+    die "@cmd failed with $rc\n" unless $rc == 0;
+}
