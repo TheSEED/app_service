9d3d5ff0f43432fcb978382ca246686a0a8cb1a3
Robert Olson <olson@mcs.anl.gov>
2018-11-09 14:40:19 -0600

    Use NFS tmpdir for large runs.

--- scripts/App-RNASeq.pl
+++ scripts/App-RNASeq.pl
@@ -36,22 +36,35 @@ sub process_rnaseq {
     print "Proc RNASeq ", Dumper($app_def, $raw_params, $params);
     my $time1 = `date`;
 
+    #
+    # Redirect tmp to large NFS if more than 4 input files.
+    # (HACK)
+    #
+    my $file_count = count_params_files($params);
+    print STDERR "File count: $file_count\n";
+    my $bigtmp = "/vol/patric3/tmp";
+    if ($file_count > 4 && -d $bigtmp)
+    {
+	print STDERR "Changing tmp from $ENV{TEMPDIR} to $bigtmp\n";
+	$ENV{TEMPDIR} = $ENV{TMPDIR} = $bigtmp;
+    }
+    
     $global_token = $app->token();
     $global_ws = $app->workspace;
-
+    
     my $output_folder = $app->result_folder();
     # my $output_base   = $params->{output_file};
-
+    
     my $recipe = $params->{recipe};
-
+    
     # my $tmpdir = File::Temp->newdir();
     my $tmpdir = File::Temp->newdir( CLEANUP => 1 );
     # my $tmpdir = "/tmp/RNApubref";
     # my $tmpdir = "/tmp/RNAuser";
-    system("chmod 755 $tmpdir");
+    system("chmod", "755", "$tmpdir");
     print STDERR "$tmpdir\n";
     $params = localize_params($tmpdir, $params);
-
+    
     my @outputs;
     my $prefix = $recipe;
     my $host = 0;
@@ -67,9 +80,9 @@ sub process_rnaseq {
     } else {
         die "Unrecognized recipe: $recipe \n";
     }
-
+    
     print STDERR '\@outputs = '. Dumper(\@outputs);
-
+    
     #
     # Create folders first.
     #
@@ -114,7 +127,7 @@ sub process_rnaseq {
     {
 	my($ofile, $type) = @$output;
 	next if $type eq 'folder';
-
+	
 	if (! -f $ofile)
 	{
 	    warn "Output file '$ofile' of type '$type' does not exist\n";
@@ -146,23 +159,23 @@ sub run_rna_rocket {
     my ($params, $tmpdir, $host) = @_;
     
     my $cwd = getcwd();
-
+    
     my $json = JSON::XS->new->pretty(1);
     #
     # Write job description.
     #
     my $jdesc = "$cwd/jobdesc.json";
     write_file($jdesc, $json->encode($params));
-
+    
     my $data_api = Bio::KBase::AppService::AppConfig->data_api_url;
     my $dat = { data_api => "$data_api/genome_feature" };
     #
     # no pretty, ensure it's on one line
     #
     my $sstring = encode_json($dat);
-
+    
     my $outdir = "$tmpdir/Rocket";
-
+    
     my $exps     = params_to_exps($params);
     my $labels   = $params->{experimental_conditions};
     my $ref_id   = $params->{reference_genome_id} or die "Reference genome is required for RNA-Rocket\n";
@@ -172,13 +185,13 @@ sub run_rna_rocket {
     my $diffexp_folder = "$outdir/.$output_name$dsuffix";
     my $diffexp_file = "$outdir/$output_name$dsuffix";
     my $ref_dir  = prepare_ref_data_rocket($ref_id, $tmpdir, $host);
-
+    
     print "Run rna_rocket ", Dumper($exps, $labels, $tmpdir);
-
+    
     # my $rocket = "/home/fangfang/programs/Prok-tuxedo/prok_tuxedo.py";
     my $rocket = "prok_tuxedo.py";
     verify_cmd($rocket);
-
+    
     my @cmd = ($rocket);
     if ($host) {
         push @cmd, ("--index");
@@ -188,12 +201,12 @@ sub run_rna_rocket {
     push @cmd, ("-d", $diffexp_name);
     push @cmd, ("--jfile", $jdesc);
     push @cmd, ("--sstring", $sstring);
-
+    
     #push @cmd, ("-L", join(",", map { s/^\W+//; s/\W+$//; s/\W+/_/g; $_ } @$labels)) if $labels && @$labels;
     #push @cmd, map { my @s = @$_; join(",", map { join("%", @$_) } @s) } @$exps;
-
+    
     print STDERR "cmd = ", join(" ", @cmd) . "\n\n";
-
+    
     #
     # Run directly with IPC::Run so that stdout/stderr can flow in realtime to the
     # output collection infrastructure.
@@ -203,20 +216,20 @@ sub run_rna_rocket {
     {
 	die "Error $? running @cmd\n";
     }
-
-#    my ($rc, $out, $err) = run_cmd(\@cmd);
-#    print STDERR "STDOUT:\n$out\n";
-#    print STDERR "STDERR:\n$err\n";
     
-
-
+    #    my ($rc, $out, $err) = run_cmd(\@cmd);
+    #    print STDERR "STDOUT:\n$out\n";
+    #    print STDERR "STDERR:\n$err\n";
+    
+    
+    
     run("echo $outdir && ls -ltr $outdir");
-
+    
     #
     # Collect output and assign types.
     #
     my @outputs;
-
+    
     #
     # BAM/BAI/GTF files are in the replicate folders.
     # We flatten the file structure in replicate folders for the
@@ -253,7 +266,7 @@ sub run_rna_rocket {
 	    }
 	}
     }
-
+    
     #
     # Remaining files are loaded as plain text.
     #
@@ -261,37 +274,37 @@ sub run_rna_rocket {
     {
 	push(@outputs, [$txt, 'txt']);
     }
-
+    
     push @outputs, [ "$outdir/$ref_id/gene_exp.gmx", 'diffexp_input_data' ] if -s "$outdir/$ref_id/gene_exp.gmx";
     push @outputs, [ $diffexp_file, 'job_result' ] if -s $diffexp_file;
     push @outputs, [ $diffexp_folder, 'folder' ] if -e $diffexp_folder and -d $diffexp_folder;
-
+    
     return @outputs;
 }
 
 sub run_rockhopper {
     my ($params, $tmpdir) = @_;
-
+    
     my $exps     = params_to_exps($params);
     my $labels   = $params->{experimental_conditions};
     my $stranded = defined($params->{strand_specific}) && !$params->{strand_specific} ? 0 : 1;
     my $ref_id   = $params->{reference_genome_id};
     my $ref_dir  = prepare_ref_data($ref_id, $tmpdir) if $ref_id;
-
+    
     print "Run rockhopper ", Dumper($exps, $labels, $tmpdir);
-
+    
     # my $jar = "/home/fangfang/programs/Rockhopper.jar";
     my $jar = $ENV{KB_RUNTIME} . "/lib/Rockhopper.jar";
     -s $jar or die "Could not find Rockhopper: $jar\n";
-
+    
     my $outdir = "$tmpdir/Rockhopper";
-
+    
     my @cmd = (qw(java -Xmx1200m -cp), $jar, "Rockhopper");
-
+    
     print STDERR '$exps = '. Dumper($exps);
-
+    
     my @conditions = clean_labels($labels);
-
+    
     push @cmd, qw(-SAM -TIME);
     push @cmd, qw(-s false) unless $stranded;
     push @cmd, ("-p", 1);
@@ -299,15 +312,15 @@ sub run_rockhopper {
     push @cmd, ("-g", $ref_dir) if $ref_dir;
     push @cmd, ("-L", join(",", @conditions)) if $labels && @$labels;
     push @cmd, map { my @s = @$_; join(",", map { join("%", @$_) } @s) } @$exps;
-
+    
     print STDERR "cmd = ", join(" ", @cmd) . "\n\n";
-
+    
     my ($rc, $out, $err) = run_cmd(\@cmd);
     print STDERR "STDOUT:\n$out\n";
     print STDERR "STDERR:\n$err\n";
-
+    
     run("echo $outdir && ls -ltr $outdir");
-
+    
     my @outputs;
     if ($ref_id) {
         @outputs = merge_rockhoppper_results($outdir, $ref_id, $ref_dir);
@@ -317,21 +330,21 @@ sub run_rockhopper {
         my @files = glob("$outdir/*.txt");
         @outputs = map { [ $_, 'txt' ] } @files;
     }
-
+    
     return @outputs;
 }
 
 sub make_diff_exp_gene_matrix {
     my ($dir, $ref_id, $conditions) = @_;
-
+    
     my $transcript = "$dir/$ref_id\_transcripts.txt";
     my $num = scalar@$conditions;
     return unless -s $transcript && $num > 1;
-
+    
     my @genes;
     my %hash;
     my @comps;
-
+    
     my @lines = `cat $transcript`;
     shift @lines;
     my $comps_built;
@@ -351,7 +364,7 @@ sub make_diff_exp_gene_matrix {
         }
         $comps_built = 1;
     }
-
+    
     my $outf = "$dir/$ref_id\_gene_exp.gmx";
     my @outlines;
     push @outlines, join("\t", 'Gene ID', @comps);
@@ -362,7 +375,7 @@ sub make_diff_exp_gene_matrix {
     }
     my $out = join("\n", @outlines)."\n";
     write_output($out, $outf);
-
+    
     return $outf;
 }
 
@@ -387,13 +400,13 @@ sub clean_labels {
 sub merge_rockhoppper_results {
     my ($dir, $gid, $ref_dir_str) = @_;
     my @outputs;
-
+    
     my @ref_dirs = split(/,/, $ref_dir_str);
     my @ctgs = map { s/.*\///; $_ } @ref_dirs;
-
+    
     my %types = ( "transcripts.txt" => 'txt',
-                  "operons.txt"     => 'txt' );
-
+		 "operons.txt"     => 'txt' );
+    
     for my $result (keys %types) {
         my $type = $types{$result};
         my $outf = join("_", "$dir/$gid", $result);
@@ -408,7 +421,7 @@ sub merge_rockhoppper_results {
         write_output($out, $outf);
         push @outputs, [ $outf, $type ];
     }
-
+    
     my @sams = glob("$dir/*.sam");
     for my $f (@sams) {
         my $sam = basename($f);
@@ -425,17 +438,17 @@ sub merge_rockhoppper_results {
         push @outputs, [ "$bam.bai", 'bai' ];
     }
     push @outputs, ["$dir/summary.txt", 'txt'];
-
+    
     return @outputs;
 }
 
 sub prepare_ref_data_rocket {
     my ($gid, $basedir, $host) = @_;
     $gid or die "Missing reference genome id\n";
-
+    
     my $dir = "$basedir/$gid";
     system("mkdir -p $dir");
-
+    
     if ($host){
         my $tar_url = "ftp://ftp.patricbrc.org/genomes/$gid/$gid.RefSeq.ht2.tar";
         my $out = curl_file($tar_url,"$dir/$gid.RefSeq.ht2.tar");
@@ -444,19 +457,19 @@ sub prepare_ref_data_rocket {
         my $gff_url = "ftp://ftp.patricbrc.org/genomes/$gid/$gid.RefSeq.gff";
         $out = curl_file($gff_url,"$dir/$gid.RefSeq.gff");
     }
-
+    
     else{
         my $api_url = "$data_url/genome_feature/?and(eq(genome_id,$gid),eq(annotation,PATRIC),or(eq(feature_type,CDS),eq(feature_type,tRNA),eq(feature_type,rRNA)))&sort(+accession,+start,+end)&http_accept=application/cufflinks+gff&limit(25000)";
         my $ftp_url = "ftp://ftp.patricbrc.org/genomes/$gid/$gid.PATRIC.gff";
-
+	
         my $url = $api_url;
         # my $url = $ftp_url;
         my $out = curl_text($url);
         write_output($out, "$dir/$gid.gff");
-
+	
         $api_url = "$data_url/genome_sequence/?eq(genome_id,$gid)&http_accept=application/sralign+dna+fasta&limit(25000)";
         $ftp_url = "ftp://ftp.patricbrc.org/genomes/$gid/$gid.fna";
-
+	
         $url = $api_url;
         # $url = $ftp_url;
         my $out = curl_text($url);
@@ -464,36 +477,36 @@ sub prepare_ref_data_rocket {
         $out =~ s/\n+/\n/g;
         write_output($out, "$dir/$gid.fna");
     }
-
+    
     return $dir;
 }
 
 sub prepare_ref_data {
     my ($gid, $basedir) = @_;
     $gid or die "Missing reference genome id\n";
-
+    
     my $url = "$data_url/genome_sequence/?eq(genome_id,$gid)&select(accession,genome_name,description,length,sequence)&sort(+accession)&http_accept=application/json&limit(25000)";
     my $json = curl_json($url);
     # print STDERR '$json = '. Dumper($json);
     my @ctgs = map { $_->{accession} } @$json;
     my %hash = map { $_->{accession} => $_ } @$json;
-
+    
     $url = "$data_url/genome_feature/?and(eq(genome_id,$gid),eq(annotation,PATRIC),eq(feature_type,CDS))&select(accession,start,end,strand,aa_length,patric_id,protein_id,gene,refseq_locus_tag,figfam_id,product)&sort(+accession,+start,+end)&limit(25000)&http_accept=application/json";
     $json = curl_json($url);
-
+    
     for (@$json) {
         my $ctg = $_->{accession};
         push @{$hash{$ctg}->{cds}}, $_;
     }
-
+    
     $url = "$data_url/genome_feature/?and(eq(genome_id,$gid),eq(annotation,PATRIC),or(eq(feature_type,tRNA),eq(feature_type,rRNA)))&select(accession,start,end,strand,na_length,patric_id,protein_id,gene,refseq_locus_tag,figfam_id,product)&sort(+accession,+start,+end)&limit(25000)&http_accept=application/json";
     $json = curl_json($url);
-
+    
     for (@$json) {
         my $ctg = $_->{accession};
         push @{$hash{$ctg}->{rna}}, $_;
     }
-
+    
     my @dirs;
     for my $ctg (@ctgs) {
         my $dir = "$basedir/$gid/$ctg";
@@ -502,52 +515,52 @@ sub prepare_ref_data {
         my $cds = $ent->{cds};
         my $rna = $ent->{rna};
         my $desc = $ent->{description} || join(" ", $ent->{genome_name}, $ent->{accession});
-
+	
         # Rockhopper only parses FASTA header of the form: >xxx|xxx|xxx|xxx|ID|
         my $fna = join("\n", ">genome|$gid|accn|$ctg|   $desc   [$ent->{genome_name}]",
                        uc($ent->{sequence}) =~ m/.{1,60}/g)."\n";
-
+	
         my $ptt = join("\n", "$desc - 1..$ent->{length}",
-                             scalar@{$ent->{cds}}.' proteins',
-                             join("\t", qw(Location Strand Length PID Gene Synonym Code FIGfam Product)),
-                             map { join("\t", $_->{start}."..".$_->{end},
-                                              $_->{strand},
-                                              $_->{aa_length},
-                                              $_->{patric_id} || $_->{protein_id},
-                                              # $_->{refseq_locus_tag},
-                                              $_->{patric_id},
-                                              # $_->{gene},
-                                              join("/", $_->{refseq_locus_tag}, $_->{gene}),
-                                              '-',
-                                              $_->{figfam_id},
-                                              $_->{product})
-                                            } @$cds
+		       scalar@{$ent->{cds}}.' proteins',
+		       join("\t", qw(Location Strand Length PID Gene Synonym Code FIGfam Product)),
+		       map { join("\t", $_->{start}."..".$_->{end},
+				  $_->{strand},
+				  $_->{aa_length},
+				  $_->{patric_id} || $_->{protein_id},
+				  # $_->{refseq_locus_tag},
+				  $_->{patric_id},
+				  # $_->{gene},
+				  join("/", $_->{refseq_locus_tag}, $_->{gene}),
+				  '-',
+				  $_->{figfam_id},
+				  $_->{product})
+			     } @$cds
                       )."\n" if $cds && @$cds;
-
+	
         my $rnt = join("\n", "$desc - 1..$ent->{length}",
-                             scalar@{$ent->{rna}}.' RNAs',
-                             join("\t", qw(Location Strand Length PID Gene Synonym Code FIGfam Product)),
-                             map { join("\t", $_->{start}."..".$_->{end},
-                                              $_->{strand},
-                                              $_->{na_length},
-                                              $_->{patric_id} || $_->{protein_id},
-                                              # $_->{refseq_locus_tag},
-                                              $_->{patric_id},
-                                              # $_->{gene},
-                                              join("/", $_->{refseq_locus_tag}, $_->{gene}),
-                                              '-',
-                                              $_->{figfam_id},
-                                              $_->{product})
-                                            } @$rna
+		       scalar@{$ent->{rna}}.' RNAs',
+		       join("\t", qw(Location Strand Length PID Gene Synonym Code FIGfam Product)),
+		       map { join("\t", $_->{start}."..".$_->{end},
+				  $_->{strand},
+				  $_->{na_length},
+				  $_->{patric_id} || $_->{protein_id},
+				  # $_->{refseq_locus_tag},
+				  $_->{patric_id},
+				  # $_->{gene},
+				  join("/", $_->{refseq_locus_tag}, $_->{gene}),
+				  '-',
+				  $_->{figfam_id},
+				  $_->{product})
+			     } @$rna
                       )."\n" if $rna && @$rna;
-
+	
         write_output($fna, "$dir/$ctg.fna");
         write_output($ptt, "$dir/$ctg.ptt") if $ptt;
         write_output($rnt, "$dir/$ctg.rnt") if $rnt;
-
+	
         push(@dirs, $dir) if $ptt;
     }
-
+    
     return join(",",@dirs);
 }
 
@@ -620,6 +633,19 @@ sub localize_params {
     return $params;
 }
 
+sub count_params_files {
+    my ($params) = @_;
+    my $count = 0;
+    if (ref($params->{paired_end_libs}))
+    {
+	$count += 2 * @{$params->{paired_end_libs}};
+    }
+    if (ref($params->{single_end_libs}))
+    {
+	$count += @{$params->{single_end_libs}};
+    }
+    return $count;
+}
 
 sub get_ws {
     return $global_ws;
@@ -634,11 +660,11 @@ sub get_ws_file {
     # return $id; # DEBUG
     my $ws = get_ws();
     my $token = get_token();
-
+    
     my $base = basename($id);
     my $file = "$tmpdir/$base";
     # return $file; # DEBUG
-
+    
     my $fh;
     open($fh, ">", $file) or die "Cannot open $file for writing: $!";
 
