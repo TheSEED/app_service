010a23f5d2db2e526c1a26c3c887e4c790231158
Robert Olson <olson@mcs.anl.gov>
2019-12-18 16:30:43 -0600

    Add RASTJob application

--- service-scripts/App-RASTJob.pl
+++ service-scripts/App-RASTJob.pl
@@ -21,11 +21,48 @@ use IO::File;
 use Module::Metadata;
 use GenomeTypeObject;
 
-my $script = Bio::KBase::AppService::AppScript->new(\&process_genome);
+my $script = Bio::KBase::AppService::AppScript->new(\&process_genome, \&preflight);
 
 my $rc = $script->run(\@ARGV);
 
 exit $rc;
+
+sub preflight
+{
+    my($app, $app_def, $raw_params, $params) = @_;
+
+    #
+    # Do some sanity checking on params.
+    #
+    # Both recipe and workflow may not be specified.
+    #
+    if ($params->{workflow} && $params->{recipe})
+    {
+	die "Both a workflow document and a recipe may not be supplied to an annotation request";
+    }
+
+    #
+    # Ensure the contigs are valid, and look up their size.
+    #
+
+    my $gto = $params->{genome_object};
+    $gto or die "Genome object must be specified\n";
+
+    my $res = $app->workspace->stat($gto);
+    $res->size > 0 or die "Genome object $gto not found\n";
+
+    my $time = 7200;
+
+    #
+    # Request 8 cpus for some of the fatter bits of the compute.
+    #
+    return {
+	cpu => 8,
+	memory => "8G",
+	runtime => int($time),
+	storage => 0,
+    };
+}
 
 sub process_genome
 {
@@ -75,18 +112,6 @@ sub process_genome
     }
 	
     $result = $core->run_pipeline($genome, $params->{workflow}, $params->{recipe}, $override);
-
-    #
-    # Use the GenomeTypeObject code to compute overall genome metrics.
-    #
-    
-    my $metrics = GenomeTypeObject::metrics($result);
-    %{$result->{quality}->{genome_metrics}} = %$metrics;
-
-    {
-	local $Bio::KBase::GenomeAnnotation::Service::CallContext = $core->ctx;
-	$result = $core->impl->compute_genome_quality_control($result);
-    }
 
     #
     # We don't use the core write_output here because all we need is the
