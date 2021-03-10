9db21ef62e9384d97910e1f76c3c9f238188ac82
Robert Olson <olson@mcs.anl.gov>
2021-03-10 16:24:40 -0600

    bring settings over from production.

--- lib/Bio/KBase/AppService/Scheduler.pm
+++ lib/Bio/KBase/AppService/Scheduler.pm
@@ -97,7 +97,8 @@ sub new
 	%opts,
     };
 
-    $redis->command("subscribe", "task_submission",
+    # disable subscribe for now
+    $redis->command("subscribe", "task_submissionx",
 		    sub {
 			my($result, $error) = @_;
 			if ($error)
@@ -473,7 +474,7 @@ sub task_start_check
     # This is a crude measure, as we'll do this again each time through. It might help in
     # rate limiting very large submissions.
     #
-    my $max_per_owner_release = 500;
+    my $max_per_owner_release = 50;
     my %jobs_released_per_owner;
 
     my $cluster = $self->default_cluster;
@@ -491,7 +492,7 @@ sub task_start_check
     # Also query for the number of submitted jobs per user, and apply a limit there.
     # The returns here are the users who have too many jobs submitted already.
     #
-    my $per_user_limit = 20;
+    my $per_user_limit = 100;
     my $res = $self->db->dbh->selectall_arrayref(qq(SELECT t.owner, COUNT(t.id)
 						    FROM Task t
 						       JOIN TaskExecution te ON t.id = te.task_id
