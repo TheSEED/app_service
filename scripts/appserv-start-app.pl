138e560f2a0405de0290d4d386b471138c08e5da
Robert Olson <olson@mcs.anl.gov>
2021-03-24 12:59:40 -0500

    Add --container-id and --base-url to start-app script.

--- scripts/appserv-start-app.pl
+++ scripts/appserv-start-app.pl
@@ -5,15 +5,17 @@ use Data::Dumper;
 use JSON::XS;
 use File::Slurp;
 
-my($opt, $usage) = describe_options("%c %o app-id params-data workspace",
+my($opt, $usage) = describe_options("%c %o app-id params-data [workspace]",
 				    ["output-path|d=s", "Change the output path"],
 				    ["output-file|f=s", "Change the output file"],
+				    ["container-id|c=s", "Use the specified container"],
+				    ["base-url|b=s", "Submit with the chosen base URL"],
 				    ["verbose|v", "Show verbose output"],
 				    ["url|u=s", "Service URL"],
 				    ["help|h", "Show this help message"]);
 
 print($usage->text), exit if $opt->help;
-print($usage->text), exit 1 if (@ARGV != 3);
+print($usage->text), exit 1 if @ARGV != 3 && @ARGV != 2;
 
 my $client = Bio::KBase::AppService::Client->new($opt->url);
 
@@ -23,6 +25,12 @@ my $workspace = shift;
 
 my $params = decode_json(scalar read_file($params_data));
 
+my $start_params = {
+    defined($workspace) ? (workspace => $workspace) : (),
+    $opt->container_id ? (container_id => $opt->container_id) : (),
+    $opt->base_url ? (base_url => $opt->base_url) : (),
+};
+    
 if ($params->{output_path} && $opt->output_path)
 {
     print "Change output path from $params->{output_path} to " . $opt->output_path . "\n";
@@ -35,7 +43,7 @@ if ($params->{output_file} && $opt->output_file)
     $params->{output_file} = $opt->output_file;
 }
 
-my $task = $client->start_app($app_id, $params, $workspace);
+my $task = $client->start_app2($app_id, $params, $start_params);
 
 if ($opt->verbose)
 {
