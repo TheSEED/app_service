b27823c759ae237054159167dd1b7a1bae853359
Robert Olson <olson@mcs.anl.gov>
2021-11-08 14:28:25 -0600

    monthly report update

--- service-scripts/p3x-monthly-report.pl
+++ service-scripts/p3x-monthly-report.pl
@@ -10,8 +10,8 @@ my $dbh = $db->dbh;
 
 my %app_values;
 
-my $start = '2021-09-01 00:00:00';
-my $end = '2021-10-01:00:00:00';
+my $start = '2021-10-01 00:00:00';
+my $end = '2021-11-01:00:00:00';
 my $include_staff = 1;
 
 #
