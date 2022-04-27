2582c86e378426fa7e0b35eb50b1b08501af86b1
Robert Olson <olson@mcs.anl.gov>
2022-04-27 11:06:29 -0500

    Add support for specifying month on the command line. Add --output / -o for writing output to a file instead of stdout.

--- service-scripts/p3x-monthly-report.pl
+++ service-scripts/p3x-monthly-report.pl
@@ -1,8 +1,51 @@
+=head1 NAME
+
+    p3x-monthly-report - Generate NIAID monthly report data for task completionstatus
+
+=head1 SYNOPSIS
+
+    p3x-monthly-report YYYY-MM > report.txt
+
+=head1 DESCRIPTION
+
+Generate summary data for the given month.
+
+We assume the data is live in the Task tables in the database 
+and not archived in the historical data. If we wish to look back to the historical 
+data this script will need to be modified to do so.
+
+=cut
 
 use Data::Dumper;
 use strict;
+use FileHandle;
 use Bio::KBase::AppService::SchedulerDB;
 use List::MoreUtils qw(first_index);
+use Getopt::Long::Descriptive;
+
+my($opt, $usage) = describe_options("%c %o YYYY-MM",
+				    ["output|o=s" => "Write output to this file instead of stdout"],
+				    ["help|h" => "Show this help message"]);
+print($usage->text), exit if $opt->help;
+die($usage->text) unless @ARGV == 1;
+
+my $month_str = shift;
+
+my($year, $month) = $month_str =~ m!^(\d{4})[/-](\d{1,2})$!;
+if (!$year || !$month || $year !~ /^20/ || $month < 0 || $month > 12)
+{
+    die "Invalid month specfiication '$month_str'\n";
+}
+
+my $end_year = $year;
+my $end_month = $month + 1;
+$end_year++ if $end_month > 12;
+
+my $start = sprintf("%04d-%02d-01", $year, $month);
+my $end = sprintf("%04d-%02d-01", $end_year, $end_month);
+
+my $start_ts = "$start:00:00:00";
+my $end_ts = "$end:00:00:00";
 
 my $db = Bio::KBase::AppService::SchedulerDB->new();
 
@@ -10,22 +53,27 @@ my $dbh = $db->dbh;
 
 my %app_values;
 
-my $start = '2022-02-01:00:00:00';
-my $end = '2022-03-01:00:00:00';
 my $include_staff = 1;
 
-print "Service report for the period $start - $end\n";
-print "\n";
+my $out_fh = \*STDOUT;
+if ($opt->output)
+{
+    $out_fh = FileHandle->new($opt->output, "w");
+    $out_fh or die "Cannot open " . $opt->output . ": $!";
+}
+
+print $out_fh "Service report for the period $start - $end\n";
+print $out_fh "\n";
 #
 # Determine number of unique users submitting jobs.
 #
 my $res = $dbh->selectcol_arrayref(qq(SELECT COUNT(DISTINCT owner)
 				      FROM Task
 				      WHERE submit_time >= ? and submit_time < ?),
-				   undef, $start, $end);
+				   undef, $start_ts, $end_ts);
 my $distinct_users = $res->[0];
-print "Distinct users submitting jobs:\t$distinct_users\n";
-print "\n";
+print $out_fh "Distinct users submitting jobs:\t$distinct_users\n";
+print $out_fh "\n";
 
 
 #
@@ -95,10 +143,10 @@ while (my($app, $vals) = each %$data)
 }
 
 my @cols = qw(total completed failed frac_completed min avg max std wait_min wait_avg wait_max wait_std);
-print join("\t", 'application', @cols), "\n";
+print $out_fh join("\t", 'application', @cols), "\n";
 	   
 for my $app (sort keys %$data)
 {
     my $vals = $data->{$app};
-    print join("\t", $app, @$vals{@cols}), "\n";
+    print $out_fh join("\t", $app, @$vals{@cols}), "\n";
 }
